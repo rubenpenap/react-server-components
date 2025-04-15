ab78caa9813a24bb1fd62b7d606948cbd32d1ed9
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-15 19:01:04 -0400

    Refactor index.js to remove deprecated API calls, update data fetching to use createFromFetch, and streamline the rendering of server components.

--- exercises/02.server-components/01.problem.rsc/ui/index.js
+++ exercises/02.server-components/01.problem.rsc/ui/index.js
@@ -1,33 +1,20 @@
 import { Suspense, createElement as h, startTransition, use } from 'react'
 import { createRoot } from 'react-dom/client'
-// 💣 App is now an RSC! We don't want to import it here. We'll get it's
-// rendered result as data in a fetch request instead. Delete this import:
-import { App } from './app.js'
-// 💰 you're going to want this:
-// import { createFromFetch } from 'react-server-dom-esm/client'
+import { createFromFetch } from 'react-server-dom-esm/client'
 import { shipFallbackSrc } from './img-utils.js'
 
 const getGlobalLocation = () =>
 	window.location.pathname + window.location.search
 
 const initialLocation = getGlobalLocation()
-// 🐨 rename initialDataPromise to something more accurate
-// (💰 like initialContentFetchPromise)
-// 🐨 also replace /api with /rsc
-const initialDataPromise = fetch(`/api${initialLocation}`)
-	// 💣 we no longer process the response into JSON, instead react-server-dom-esm
-	// will process it for us. Delete this `then` call:
-	.then((r) => r.json())
+const initialContentFetchPromise = fetch(`/rsc${initialLocation}`)
 
-// 🐨 create a variable called initialContentPromise set to createFromFetch(initialContentFetchPromise)
+const initialContentPromise = createFromFetch(initialContentFetchPromise)
 
 function Root() {
-	// 💣 we no longer request data or render the App component, delete these lines:
-	const { shipId, search, ship, shipResults } = use(initialDataPromise)
-	return h(App, { shipId, search, ship, shipResults })
-	// 🐨 create a variable called content set to use(initialContentPromise)
-	// 💯 as a bonus, go ahead and console.log the content variable and check it out in the dev tools!
-	// 🐨 return the content
+	const content = use(initialContentPromise)
+	console.log(' Root 🔥 content:', content)
+	return content
 }
 
 startTransition(() => {
