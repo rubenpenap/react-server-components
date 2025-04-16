8cba08c4961da8aaf2c64a1d0de1be6cfc7408cf
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-15 21:03:57 -0400

    Refactor app.js to integrate Suspense for asynchronous loading of ShipDetails and SearchResults components, enhancing user experience with fallback UI elements.

--- exercises/02.server-components/03.problem.streaming/ui/app.js
+++ exercises/02.server-components/03.problem.streaming/ui/app.js
@@ -1,15 +1,6 @@
-// 🐨 you'll want to import Suspense from react
-import { /* Suspense, */ Fragment, createElement as h } from 'react'
-import {
-	ShipDetails,
-	// 💰 you'll want this:
-	// ShipFallback
-} from './ship-details.js'
-import {
-	SearchResults,
-	// 💰 you'll want this:
-	// SearchResultsFallback
-} from './ship-search-results.js'
+import { Fragment, Suspense, createElement as h } from 'react'
+import { ShipDetails, ShipFallback } from './ship-details.js'
+import { SearchResults, SearchResultsFallback } from './ship-search-results.js'
 
 export function App({ shipId, search }) {
 	return h(
@@ -35,13 +26,11 @@ export function App({ shipId, search }) {
 				h(
 					'ul',
 					null,
-					// 🐨 wrap this in a Suspense boundary with the fallback set to
-					// h(SearchResultsFallback)
-					// 💰 remember it's h(Component, props, child1, child2, child3)
-					// 💰 don't feel too bad if you need to reference the diff on this one
-					// it's kinda hard to go back to non-JSX after you've been used to
-					// using JSX for a while 😅
-					h(SearchResults, { shipId, search }),
+					h(
+						Suspense,
+						{ fallback: h(SearchResultsFallback) },
+						h(SearchResults, { shipId, search }),
+					),
 				),
 			),
 		),
@@ -49,8 +38,11 @@ export function App({ shipId, search }) {
 			'div',
 			{ className: 'details' },
 			shipId
-				? // 🐨 wrap this in a Suspense boundary with the fallback set to h(ShipFallback, { shipId })
-					h(ShipDetails, { shipId })
+				? h(
+						Suspense,
+						{ fallback: h(ShipFallback, { shipId }) },
+						h(ShipDetails, { shipId }),
+					)
 				: h('p', null, 'Select a ship from the list to see details'),
 		),
 	)
