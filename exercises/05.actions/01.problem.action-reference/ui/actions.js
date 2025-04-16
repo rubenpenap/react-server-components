ea1d7a586cb06d0d6f113b4422fc72ec3c1f4eb7
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-16 17:05:59 -0400

    Convert actions.js module to an RSC reference by adding 'use server' directive. Update error handling to return structured status and message responses for improved error management.

--- exercises/05.actions/01.problem.action-reference/ui/actions.js
+++ exercises/05.actions/01.problem.action-reference/ui/actions.js
@@ -1,4 +1,4 @@
-// 🐨 add 'use server' here to turn this module into an RSC reference
+'use server'
 
 import * as db from '../db/ship-api.js'
 
@@ -8,9 +8,9 @@ export async function updateShipName(previousState, formData) {
 			shipId: formData.get('shipId'),
 			shipName: formData.get('shipName'),
 		})
-		// 🐨 return a status of 'success' and a message of 'Success!'
+		return { status: 'success', message: 'Success!' }
 	} catch (error) {
 		console.error(error)
-		// 🐨 return a status of 'error' and a message of error?.message || String(error)
+		return { status: 'error', message: error?.message || String(error) }
 	}
 }
