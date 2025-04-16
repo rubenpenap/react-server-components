01fbb30c58ec6b69e0e82d03a1b8a2e8bfca7b77
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-16 15:41:11 -0400

    Refactor ShipDetailsPendingTransition component to utilize useRouter for location management and implement useSpinDelay for smoother loading state transitions, improving user experience during ship detail rendering.

--- exercises/04.router/02.problem.pending-ui/ui/ship-details-pending.js
+++ exercises/04.router/02.problem.pending-ui/ui/ship-details-pending.js
@@ -1,18 +1,16 @@
 'use client'
 
 import { createElement as h } from 'react'
-// 💰 you'll want this
-// import { parseLocationState, useRouter } from './router.js'
-// 💯 if you want to do the extra credit, grab this
-// import { useSpinDelay } from './spin-delay.js'
+import { parseLocationState, useRouter } from './router.js'
+import { useSpinDelay } from './spin-delay.js'
 
 export function ShipDetailsPendingTransition({ children }) {
-	// 🐨 get the location and nextLocation from useRouter
-	// 🐨 the details are pending if the shipId of the nextLocation differs from
-	// the shipId of the current location
-	// 💰 use parseLocationState to get the shipId.
-	// 💯 for extra credit, avoid a flash of loading state with useSpinDelay
-	const isShipDetailsPending = false
+	const { location, nextLocation } = useRouter()
+	const isShipDetailsPending = useSpinDelay(
+		parseLocationState(nextLocation).shipId !==
+			parseLocationState(location).shipId,
+		{ delay: 300, minDuration: 350 },
+	)
 
 	return h('div', {
 		className: 'details',
