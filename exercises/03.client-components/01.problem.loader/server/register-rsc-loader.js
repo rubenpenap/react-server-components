e2729a5dd775a24499004de3e87d5dfb31a77c86
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-16 11:15:06 -0400

    Remove commented-out code and streamline the registration of the custom RSC loader in register-rsc-loader.js for improved clarity and maintainability.

--- exercises/03.client-components/01.problem.loader/server/register-rsc-loader.js
+++ exercises/03.client-components/01.problem.loader/server/register-rsc-loader.js
@@ -1,5 +1,3 @@
-// 🐨 register our custom rsc-loader.js module as a custom loader
-// 💰 this isn't a node.js workshop, so here you go
-// import { register } from 'node:module'
+import { register } from 'node:module'
 
-// register('./rsc-loader.js', import.meta.url)
+register('./rsc-loader.js', import.meta.url)
